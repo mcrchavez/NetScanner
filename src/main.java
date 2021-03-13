9ea955aa0633a2a0332390655ae16fc8c5493ef1
Michael Chavez <mcrchavez713@gmail.com>
2021-03-13 14:02:52 -0500

    Added switch for args

--- src/main.java
+++ src/main.java
@@ -1,5 +1,32 @@
 public class main {
     public static void main(String[] args) {
+
+        if(args.length > 0){
+            switch(args[0]){
+                case "-h":
+                case "--help":
+                    System.out.print(String.format("-P ping sweeper:\n-v verbose\n-r specify range of ips for ping\n\n" +
+                            "-Ps port scanner:\n-h specify target hostname or ip\n-p specify range of ports to query"));
+                    break;
+                case "-P":
+                    PingSweeper ping = new PingSweeper(args);
+                    break;
+                case "-Ps":
+                    PortScanner scan = new PortScanner(args);
+                    break;
+                default:
+                    System.out.println("Invalid argument");
+                    System.out.print(String.format("-P ping sweeper:\n-v verbose\n-r specify range of ips for ping\n\n" +
+                            "-Ps port scanner:\n-h specify target hostname or ip\n-p specify range of ports to query"));
+                    break;
+            }
+
+        }
+        if(args.length == 0){
+            System.out.print(String.format("-P ping sweeper:\n-v verbose\n-r specify range of ips for ping\n\n" +
+                    "-Ps port scanner:\n-h specify target hostname or ip\n-p specify range of ports to query"));
+        }
         PortScanner test = new PortScanner(args);
+        PingSweeper test2 = new PingSweeper(args);
     }
 }
