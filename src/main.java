37c61e0a92f0e29f4851f8ccddadbf5c668dfdf3
Michael Chavez <mcrchavez713@gmail.com>
2021-06-09 12:12:48 -0400

    Created picocli terminal command interface for PingSweep and PortScanner

--- src/main.java
+++ src/main.java
@@ -13,8 +13,15 @@ import picocli.CommandLine.*;
 @Command(name="NetScanner", version = ".01 improved cmdline prototype", mixinStandardHelpOptions = true, description = "Network Enumeration Framework")
 public class main{
     @Command(name="-P", description = "Sweep network for targets using ping protocol",  mixinStandardHelpOptions = true)
-    void pingSweepSubCommand(@Option(names={"-t", "--host", "--target"}, description = "Specify hostname or target IP for sweep", defaultValue = "0") String target){
+    void pingSweepSubCommand(@Option(names={"-t", "--host", "--target"}, description = "Specify target IP for ping sweep", defaultValue = "localhost") String target,@Option(names = {"-r", "--range"}, description = "Specify range of IP addresses to ping", split = " ", defaultValue = "0 255") int[] ipRange, @Option(names = {"-v"},
+            description = "Enable Verbose output of results", defaultValue = "false") boolean verbosity){
     }
+    @Command(name="-Ps", description = "Scan specified device/IP on network for open ports", mixinStandardHelpOptions = true)
+    void PortScanSubCommand(@Option(names = {"-t", "--host", "--target"}, description = "Specify target IP/hostname for port scan", defaultValue = "localhost") String target, @Option(names = {"-r", "--range"}, description = "Specify range of ports on host to probe", split = " ", defaultValue = "0 65535") int[] portRange, @Option(names = {"-v"},
+    description = "Enable verbose output of results", defaultValue = "false") boolean verbosity){
+
+    }
+
 
 
 
